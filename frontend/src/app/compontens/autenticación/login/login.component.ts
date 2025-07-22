bc20c9b34243f84d08d43d617c1425245c8547bd
ThiagoIcochea <u22330428@utp.edu.pe>
2025-07-21 19:40:10 -0500

    backend actualizado

--- frontend/src/app/compontens/autenticación/login/login.component.ts
+++ frontend/src/app/compontens/autenticación/login/login.component.ts
@@ -47,7 +47,7 @@ export class LoginComponent implements OnInit {
     const { dni, contrasena } = this.loginForm.value;
 
     this.http
-      .post<any>('http://localhost:3000/api/auth/login', { dni, contrasena })
+      .post<any>('https://faithful-communication-production.up.railway.app/api/auth/login', { dni, contrasena })
       .subscribe({
         next: (res) => {
           localStorage.setItem('token', res.token);
